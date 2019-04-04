42ac0c0bca2fced41d64cbf2160a9b23bd84346f
Carle <carle9292@gmail.com>
2019-04-04 16:55:54 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <img src={logo} alt="logo" />
+        <h1>하이</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
